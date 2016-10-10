dd283827b88b2c63c308f0fc7eb4e95bb1d107b0
byjuanamn <juan.antonio.martin@gmail.com>
2016-10-10 23:57:27 +0200

    Populamos el modelo con containers

--- StorageIOSExplorer/ContainersTableViewController.swift
+++ StorageIOSExplorer/ContainersTableViewController.swift
@@ -25,6 +25,8 @@ class ContainersTableViewController: UITableViewController {
             
             client = account.getBlobClient()
             
+            self.readAllContainers()
+            
         } catch let error {
             print(error)
         }
@@ -104,24 +106,35 @@ class ContainersTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        if model.isEmpty {
+            return 0
+        }
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        if model.isEmpty {
+            return 0
+        }
+        return model.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELDA", for: indexPath)
 
         // Configure the cell...
+        
+        let item = model[indexPath.row]
 
+        cell.textLabel?.text = item.name
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
